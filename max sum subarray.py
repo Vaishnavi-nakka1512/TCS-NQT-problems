322ca3a8e4356ce9ddad318fb9c329c66a4b2ce8
Vaishnavi-nakka1512 <vaishnavinakka408@gmail.com>
2026-06-05 11:26:34 +0530

    Add files via upload

---
+++ max sum subarray.py
@@ -0,0 +1,11 @@
+a=list(map(int,input().split()))
+b=[]
+for i in range(len(a)):
+    max_s=a[i]
+    b.append(max_s)
+    for j in range(i+1,len(a)):
+        max_s+=a[j]
+        b.append(max_s)
+print(max(b))
+    
+
